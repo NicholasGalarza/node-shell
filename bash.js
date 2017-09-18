10a0d0cefd68200773a28364a4637b3bc95a8b9a
NicholasGalarza <nicogala93@gmail.com>
2017-09-18 09:42:17 -0400

    stream communication

---
+++ bash.js
@@ -0,0 +1,14 @@
+// console.log("Process Alone", process);
+// console.log("\nObjects Keys", Object.keys(process));
+
+// Output a prompt
+process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', function (data) {
+  var cmd = data.toString().trim(); // remove the newline
+
+  process.stdout.write('You typed: ' + cmd);
+  process.stdout.write('\nprompt > ');
+
+});
